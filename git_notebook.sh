fec73b12c7bafeb478456e7f964d9f9b655a5012
wariobrega <wariobrega@gmail.com>
2015-08-21 13:18:05 +0200

    adding git_notebook

---
+++ git_notebook.sh
@@ -0,0 +1,20 @@
+##to initialize repository inside your local direcotry
+
+git init
+
+##to add my repo to a source directory on your local machine 
+git remote add origin https://github.com/wariobrega/scrapcode.git
+
+##to clone repository inside a directory in youyr local machine 
+
+git clone https://github.com/wariobrega/scrapcode.git
+
+
+#to add files for committing into my repo
+
+git add . #inside my repo folder
+
+##to commit changes and add commit messages
+
+git commit - m 'changelog' #changelog == whatever you changed
+
