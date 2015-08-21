702c8d2391fec2c823e094f7f4e28727afc5b293
wariobrega <wariobrega@gmail.com>
2015-08-21 13:43:20 +0200

    added new info about git

--- git_notebook.sh
+++ git_notebook.sh
@@ -14,7 +14,33 @@ git clone https://github.com/wariobrega/scrapcode.git
 
 git add . #inside my repo folder
 
+#mind to use the '--all' option to add changes for ALL FILES,  in this fashon
+
+git add --all .
+
 ##to commit changes and add commit messages
 
 git commit - m 'changelog' #changelog == whatever you changed
 
+##to send changes to my git repository
+
+git push -v #with verbose option
+
+#to add a new remote repository  
+
+git remote add REPONAME REPOURL #after haviong created it on github
+
+##to configure local git settings##
+
+
+#adding username
+git config --global user.name wariobrega
+
+#adding user email
+git config --global user.email wariobrega@gmail.com
+
+#maually edit git config
+git config --global --edit
+
+##adding a default editor for git
+git config --global core.editor subl #this is sublimetext 3
